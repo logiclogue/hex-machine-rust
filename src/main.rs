099cb899db6bc1304f1b3b9f0344d4efc8c899b0
logiclogue <jordanpaullord@hotmail.co.uk>
2018-03-12 17:15:26 +0000

    Updates

--- src/main.rs
+++ src/main.rs
@@ -2,22 +2,23 @@ fn main() {
     println!("Hello, world!");
 }
 
-struct Memory(Vec<i32>);
+struct Memory(Vec<u8>);
 
 impl Memory {
-    fn read(&self, address: i32) -> i32 {
-        address
+    fn read(&self, address: u32) -> u8 {
+        42
     }
 
-    //fn write(&self, address: i32, value: i32) -> Memory {
-
-    //}
+    fn write(self, address: u32, value: u8) -> Memory {
+        self
+    }
 }
 
 #[test]
 fn memory_read_given_42_returns_42() {
-    let memory = Memory(vec![1]);
-    let address = 42;
+    let address = 145;
+    let value = 42;
+    let memory = Memory(vec![1]).write(address, value);
 
     assert_eq!(memory.read(address), 42);
 }
@@ -34,7 +35,7 @@ fn memory_read_given_1_returns_1() {
 fn sandbox() {
     let my_array = vec![42, 1, 2];
 
-    let result: i32 = my_array.iter().fold();
+    let result: u8 = my_array.iter().sum();
 
     assert_eq!(result, 45);
 }
