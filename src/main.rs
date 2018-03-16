59f688b9dd9ec8d92d98ba437512fd30b963cb25
logiclogue <jordanpaullord@hotmail.co.uk>
2018-03-16 17:23:28 +0000

    Adds constructor for the machine

--- src/main.rs
+++ src/main.rs
@@ -14,6 +14,53 @@ struct Machine {
     running: bool
 }
 
+impl Machine {
+    fn new() -> Self {
+        Machine {
+            o_reg: 0,
+            a_reg: 0,
+            b_reg: 0,
+            pc: 0,
+            running: false
+        }
+    }
+}
+
+#[test]
+fn test_machine_constructor_o_reg_0() {
+    let machine = Machine::new();
+
+    assert_eq!(machine.o_reg, 0);
+}
+
+#[test]
+fn test_machine_constructor_a_reg_0() {
+    let machine = Machine::new();
+
+    assert_eq!(machine.a_reg, 0);
+}
+
+#[test]
+fn test_machine_constructor_b_reg_0() {
+    let machine = Machine::new();
+
+    assert_eq!(machine.b_reg, 0);
+}
+
+#[test]
+fn test_machine_constructor_pc_0() {
+    let machine = Machine::new();
+
+    assert_eq!(machine.pc, 0);
+}
+
+#[test]
+fn test_machine_constructor_running_false() {
+    let machine = Machine::new();
+
+    assert_eq!(machine.running, false);
+}
+
 struct Memory([u8; 256]);
 
 impl Memory {
